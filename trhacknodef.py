db7660ced956a15df4ce9ab77ccccbb259af5061
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2021-12-21 05:54:09 +0100

    Update trhacknodef.py

--- trhacknodef.py
+++ trhacknodef.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #####DONT CHANGE THIS########
-######################
+###################### _        _   _    _    ____ _  __
+| |_ _ __| | | |  / \  / ___| |/ /_ __   ___  _ __  
+| __| '__| |_| | / _ \| |   | ' /| '_ \ / _ \| '_ \
+| |_| |  |  _  |/ ___ \ |___| . \| | | | (_) | | | |
+ \__|_|  |_| |_/_/   \_\____|_|\_\_| |_|\___/|_| |_|
 ### Script By TRHACKNOnimous
 ### www.memanon.ml
 ### Don't Change This.!!!
@@ -13,8 +17,8 @@ os.system("mkdir TRHACKNOnimous")
 os.system("mv TRHACKNOnimous/ /storage/emulated/0/")
 os.system("mv TRHACKNONscript.html /storage/emulated/0/TRHACKNOnimous/")
 print
-print("Data Sedang Di pindahkan, Silahkan Tunggu.")
+print("tu n'as plus qu'à utiliser un outil comme trhacktest, pour uploader le script que tu viens de creer.")
 os.system("sleep 5")
-print("Berhasil Di pindahkan ke /storage/emulated/0/TRHACKNOnimous/TRHACKNONscript.html")
+print("script créé dans : /storage/emulated/0/TRHACKNOnimous/TRHACKNONscript.html")
 os.system("sleep 3")
-print("[ Script Siap Di gunakan ]")
+print("[ Script en cours de chargement ]")
